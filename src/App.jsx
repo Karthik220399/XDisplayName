f25ed52cfd6ac992c9b51a156ef2cd9737cbc513
Karthik Shanmugavel <135093193+Karthik220399@users.noreply.github.com>
2024-01-23 18:24:31 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setlastName] = useState("");
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (e.target.firstName.value) {
+            setFirstName(e.target.firstName.value);
+          }
+          if (e.target.lastName.value) {
+            setlastName(e.target.lastName.value);
+          }
+        }}
+      >
+        <div>
+          <label for="firstName">First Name:</label>
+          <input required id="firstName" name="firstName" type="text" />
+        </div>
+        <div>
+          <label for="lastName">Last Name:</label>
+          <input required id="flastName" name="lastName" type="text" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div className="name">{`Full Name: ${firstname + " " + lastname}`}</div>
+    </div>
+  );
+}
