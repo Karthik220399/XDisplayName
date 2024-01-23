d350e7225f3d6d63f4c47a9b4e39d23d6c6c0d35
Karthik Shanmugavel <135093193+Karthik220399@users.noreply.github.com>
2024-01-23 19:15:23 +0000

    modigy

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
 import "./styles.css";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function App() {
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setlastName] = useState("");
+  // const [firstname, setFirstName] = useState("");
+  // const [lastname, setlastName] = useState("");
+  const [inputFirst, setInputFirst] = useState("");
+  const [inputLast, setInputLast] = useState("");
+  const [FullName, setFullName] = useState("");
+  
+  useEffect(() =>{
+     if(inputFirst.length <=0){
+       setFullName("")
+     }else if(inputLast.length <=0){
+      setFullName("")
+     }
+  },[inputFirst, inputLast])
+
+
   return (
     <div>
       <h1>Full Name Display</h1>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          if (e.target.firstName.value) {
-            setFirstName(e.target.firstName.value);
-          }
-          if (e.target.lastName.value) {
-            setlastName(e.target.lastName.value);
-          }
+            setFullName(inputFirst + " " + inputLast);
         }}
       >
         <div>
-          <label for="firstName">First Name:</label>
-          <input required id="firstName" name="firstName" type="text" />
+          <label >First Name:</label>
+          <input onChange={(e) => {
+            setInputFirst(e.target.value)}} required id="firstName" name="firstName" type="text" />
         </div>
         <div>
-          <label for="lastName">Last Name:</label>
-          <input required id="flastName" name="lastName" type="text" />
+          <label >Last Name:</label>
+          <input onChange={(e) => setInputLast(e.target.value)} required id="flastName" name="lastName" type="text" />
         </div>
         <button type="submit">Submit</button>
       </form>
-      <div className={firstname && lastname ? "name" : "hidden" }>{`Full Name: ${firstname + " " + lastname}`}</div>
+      <div className={FullName? "name" : "hidden"}>{`Full Name: ${FullName}`}</div>
     </div>
   );
 }
